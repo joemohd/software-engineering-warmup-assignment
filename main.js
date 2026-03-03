98aff37c225e8175796c386ecd8e96589d4ed995
Joe Mohd <joemohd.m@gmail.com>
2026-03-03 23:16:27 +0000

    First three functions done

--- main.js
+++ main.js
@@ -1,33 +1,45 @@
 const fs = require("fs");
 
-// ============================================================
-// Function 1: getShiftDuration(startTime, endTime)
-// startTime: (typeof string) formatted as hh:mm:ss am or hh:mm:ss pm
-// endTime: (typeof string) formatted as hh:mm:ss am or hh:mm:ss pm
-// Returns: string formatted as h:mm:ss
-// ============================================================
-function getShiftDuration(startTime, endTime) {
-    // TODO: Implement this function
+function getShiftDuration(startTime, endTime) {  
+    return secondsToTime(differenceTime(convertTo24(startTime), convertTo24(endTime)));
 }
 
-// ============================================================
-// Function 2: getIdleTime(startTime, endTime)
-// startTime: (typeof string) formatted as hh:mm:ss am or hh:mm:ss pm
-// endTime: (typeof string) formatted as hh:mm:ss am or hh:mm:ss pm
-// Returns: string formatted as h:mm:ss
-// ============================================================
 function getIdleTime(startTime, endTime) {
-    // TODO: Implement this function
+    let idleTime = 0;
+
+    // find time before 8 am
+    let differenceStart = differenceTime(convertTo24(startTime), "08:00:00");
+
+    if (differenceStart > 0) {
+        idleTime += differenceStart;
+    }
+
+    // find time after 10 pm
+    let differenceEnd = differenceTime("22:00:00", convertTo24(endTime));
+
+    if (differenceEnd > 0) {
+        idleTime += differenceEnd;
+    }
+
+    idleTime = secondsToTime(idleTime);
+
+    return idleTime;
 }
 
-// ============================================================
-// Function 3: getActiveTime(shiftDuration, idleTime)
-// shiftDuration: (typeof string) formatted as h:mm:ss
-// idleTime: (typeof string) formatted as h:mm:ss
-// Returns: string formatted as h:mm:ss
-// ============================================================
 function getActiveTime(shiftDuration, idleTime) {
-    // TODO: Implement this function
+    function formatTime(time) {
+        if (time.length !== 11) {
+            time = "0" + time;
+        }
+
+        return time;
+    }
+
+    shiftDuration = formatTime(shiftDuration);
+    idleTime = formatTime(idleTime);
+    
+    let difference = differenceTime(idleTime, shiftDuration);
+    return secondsToTime(difference);
 }
 
 // ============================================================
@@ -121,3 +133,104 @@ module.exports = {
     getRequiredHoursPerMonth,
     getNetPay
 };
+
+// ============================================================
+// HELPER FUNCTIONS
+// ============================================================
+
+function convertTo24(time) {
+    let isPM = time.toLowerCase().includes("pm");
+    let currentHour = Number(time.slice(0, time.indexOf(":")));
+    let newHour = currentHour;
+    
+    if (isPM && currentHour != 12) {
+        newHour += 12;
+    } else if (!isPM && currentHour == 12) {
+        newHour = 0;
+    }
+
+    let newTime = "";
+
+    if (newHour === 0) {
+        newTime += String("00");
+    } else {
+        if (parseInt(newHour / 10) === 0) {
+            newTime += "0";
+        }
+
+        newTime += newHour;
+    }
+
+    if (time.length === 11) {
+        newTime += time.substring(2, 8);
+    } else {
+        newTime += time.substring(1, 7);
+    }
+
+    return newTime;
+}
+
+function timeToSeconds(time) {
+    let hours = Number(time.substring(0, 2));
+    let minutes = Number(time.substring(3, 5));
+    let seconds = Number(time.substring(6, 8));
+
+    minutes += (hours * 60);
+    seconds += (minutes * 60);
+
+    return seconds;
+}
+
+function differenceTime(startTime, endTime) {
+    // convert to seconds
+    startTime = timeToSeconds(startTime);
+    endTime = timeToSeconds(endTime);
+
+    // subtract from each other
+    let difference = endTime - startTime;
+
+    return difference;
+}
+
+function secondsToTime(totalSeconds) {
+    const seconds = totalSeconds % 60;
+    const totalMinutes = Math.floor(totalSeconds / 60);
+    const minutes = totalMinutes % 60;
+    const hours = Math.floor(totalMinutes / 60);
+
+    let time = "";
+
+    if (Math.floor(hours / 10) === 0) {
+        time += "0" + String(hours);
+    } else {
+        time += String(hours);
+    }
+
+    time += ":";
+
+    if (Math.floor(minutes / 10) === 0) {
+        time += "0" + String(minutes);
+    } else {
+        time += String(minutes);
+    }
+
+    time += ":";
+
+    if (Math.floor(seconds / 10) === 0) {
+        time += "0" + String(seconds);
+    } else {
+        time += String(seconds);
+    }
+
+    return time;
+}
+
+function main() {
+    let shiftDuration =  "5:00:10";
+    let idleTime = "2:30:00";
+    let activeTime = getActiveTime(shiftDuration, idleTime);
+
+    console.log(activeTime);
+}
+
+main();
